a1fb767c0b12dce13943e728253d570a46b95d81
Jakukyo Friel <weakish@gmail.com>
2016-09-15 23:08:30 +0800

    Add api doc.

---
+++ api/resources/index.js
@@ -0,0 +1,9 @@
+var index = [
+{'name': 'io.github.weakish.cacography', 'type': 'package', 'url': 'index.html#section-package', 'doc': '<p>Demostration only.</p>', 'tags': [],'icons': ['icon-package']},
+{'name': 'render', 'type': 'function', 'url': 'index.html#render', 'doc': '<p>The entry point.</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'Processor', 'type': 'alias', 'url': 'index.html#Processor', 'doc': '<p>A processing function takes command and its parameter, returns processed text.</p>', 'tags': [],'icons': ['icon-type-alias']},
+{'name': 'default_processor', 'type': 'function', 'url': 'index.html#default_processor', 'doc': '<p>Default processing function, only process <a class=\'link\' href=\'index.html#verbatim\' title=\'Go to io.github.weakish.cacography::verbatim\'><code><span class=\'identifier\'>verbatim()</span></code></a>.\nR</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'verbatim', 'type': 'function', 'url': 'index.html#verbatim', 'doc': '<p>Returns parameter text verbatim.\nR</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'io.github.weakish.cacography', 'type': 'module', 'url': 'index.html', 'doc': '<p>A demostration implementation of cacography template engine.</p>', 'tags': [],'icons': ['icon-module']}];
+var tagIndex = [
+];
